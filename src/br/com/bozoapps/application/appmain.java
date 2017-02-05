1ca3dfdd21d6f6c19e622d2cddebd8f2f2465ba1
feliperzk <felipesantosfx@gmail.com>
2017-02-05 15:34:37 -0200

    first commit

---
+++ src/br/com/bozoapps/application/appmain.java
@@ -0,0 +1,10 @@
+package br.com.bozoapps.application;
+
+public class appmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello bronzo");
+	}
+
+}
